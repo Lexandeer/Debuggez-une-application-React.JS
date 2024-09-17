64e6a698bbcb779e6f1b74dec5ad7ab51236b3d0
Alexandre <alexmiquel888@gmail.com>
2024-09-17 11:18:30 +0200

    Correction de bug
    Correction au niveau de la fonction filteredEvents, l'opérateur ternaire renvoyait les mêmes events, peu importe le type sélectionné.

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -13,9 +13,10 @@ const EventList = () => {
   const { data, error } = useData();
   const [type, setType] = useState();
   const [currentPage, setCurrentPage] = useState(1);
+
   const filteredEvents = (
-    (!type
-      ? data?.events
+    (type
+      ? data?.events.filter((evt) => evt.type === type) // J'ai rajouté un filtrage suplémentaire qui compare le type selectionné(si selctionné) et les types des events pour afficher uniquement les bons events.
       : data?.events) || []
   ).filter((event, index) => {
     if (
@@ -26,10 +27,12 @@ const EventList = () => {
     }
     return false;
   });
+
   const changeType = (evtType) => {
     setCurrentPage(1);
     setType(evtType);
   };
+  
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
   const typeList = new Set(data?.events.map((event) => event.type));
   return (
