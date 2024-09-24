ff8e89b91e7c5c7eb5c162aea409a2347396dfc5
Alexandre <alexmiquel888@gmail.com>
2024-09-24 10:16:19 +0200

    Ajout de tests unitaires
    AJout de tests unitaires pour les 3 ancres du Menu

--- src/containers/Menu/index.test.js
+++ src/containers/Menu/index.test.js
@@ -10,7 +10,7 @@ describe("When Menu is created", () => {
     await screen.findByText("Contact");
   });
 
-  describe("and a click is triggered on contact button", () => {
+  describe("and a click is triggered on contact button", () => {  // Bouton Contact
     it("document location  href change", async () => {
       render(<Menu />);
       fireEvent(
@@ -20,7 +20,31 @@ describe("When Menu is created", () => {
           bubbles: true,
         })
       );
-      expect(window.document.location.hash).toEqual("#contact");
+      expect(window.document.location.hash).toEqual("#contact");  
+    });
+  });
+
+  describe("and a click is triggered on nos-services link", () => { // Bouton nos-services
+    it("href of Nos services link is correct", async () => {
+      render(<Menu />);
+      const link = await screen.findByText("Nos services");
+      expect(link).toHaveAttribute("href", "#nos-services");
+    });
+  });
+
+  describe("and a click is triggered on nos-realisations link", () => { // Bouton nos-realisationss
+    it("href of Nos réalisations link is correct", async () => {
+      render(<Menu />);
+      const link = await screen.findByText("Nos réalisations");
+      expect(link).toHaveAttribute("href", "#nos-realisations");
+    });
+  });
+
+  describe("and a click is triggered on notre-equipe link", () => { // Bouton notre-equipe
+    it("href of Notre équipe link is correct", async () => {
+      render(<Menu />);
+      const link = await screen.findByText("Notre équipe");
+      expect(link).toHaveAttribute("href", "#notre-equipe");
     });
   });
 });
